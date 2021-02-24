c7e368726e60b2fb681f312703d49897eb87c5f0
Johnathan Monsalve <desarrollo.gi1@udea.edu.co>
2021-02-24 00:15:47 -0500

    Init

---
+++ src/main/java/com/johnathanmb/CartTul/CartTulApplication.java
@@ -0,0 +1,13 @@
+package com.johnathanmb.CartTul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartTulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartTulApplication.class, args);
+	}
+
+}
